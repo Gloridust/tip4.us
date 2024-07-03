354fe37ba6a001ea4eceb206cc8088f1a0f45d1c
Gloridust <Gloridust@gmail.com>
2024-07-03 17:35:53 +0800

    Update server.py

--- server.py
+++ server.py
@@ -5,28 +5,37 @@ from config import GIT_REPO_PATH, GIT_COMMIT_MESSAGE
 import requests
 from bs4 import BeautifulSoup
 
-import twint
+from requests_html import HTMLSession
+
 
 def fetch_tweet_details(tweet_url):
+    session = HTMLSession()
+    response = session.get(tweet_url)
+    
+    # 等待页面的JavaScript加载完毕
+    response.html.render(timeout=20)
+
     try:
-        # 从URL中提取推文ID
-        tweet_id = tweet_url.split('/')[-1]
-
-        # 使用twint获取推文信息
-        c = twint.Config()
-        c.Tweet_id = tweet_id
-        c.Hide_output = True
-        c.Store_object = True
-
-        twint.run.Lookup(c)
-        tweet = twint.output.tweets_list[0]
-
+        # 获取博主用户名
+        username_tag = response.html.find('div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-dnmrzs', first=True)
+        username = username_tag.text if username_tag else 'N/A'
+        
+        # 获取推文内容
+        content_tag = response.html.find('div[data-testid="tweetText"]', first=True)
+        content = content_tag.text if content_tag else 'N/A'
+        
+        # 获取推文中的图片
+        images = []
+        image_tags = response.html.find('img[src*="twimg.com/media"]')
+        for img in image_tags:
+            images.append(img.attrs['src'])
+        
         tweet_details = {
-            'username': tweet.username,
-            'content': tweet.tweet,
-            'images': tweet.photos
+            'username': username,
+            'content': content,
+            'images': images
         }
-
+        
         return tweet_details
     except Exception as e:
         print(f"Error: {e}")
@@ -64,6 +73,7 @@ def git_commit_post(filename):
 
 
 if __name__ == "__main__":
+    
     details = fetch_tweet_details("https://x.com/yetone/status/1807784086854918506")
 
     if details:
